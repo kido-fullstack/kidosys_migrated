7db3d198372a5cf61d8724ceffd68617f75bdfc4
ziauddin-sayyed <sayyedzia5@gmail.com>
2024-01-09 17:17:46 +0530

    Updates

--- config/index.js
+++ config/index.js
@@ -12,12 +12,12 @@ module.exports = Object.assign({}, {
     },
     db: {
       devDb: {
-        server: process.env.DB_SERVER_NAME || "65.0.69.203",
+        server: process.env.DB_SERVER_NAME || "localhost",
         port: process.env.DB_PORT || 27017,
-        username: process.env.DB_USERNAME || "kidosys",
-        password: process.env.DB_PASSWORD || "kidoYwOyv4lzsys",
+        username: process.env.DB_USERNAME || "",
+        password: process.env.DB_PASSWORD || "",
         dbName: "test",
-        uri: "mongodb://kidosys:kidoYwOyv4lzsys@65.0.69.203:27017/kidoProduction?authMechanism=DEFAULT&authSource=admin",
+        uri: "mongodb+srv://kido_admin:q8HeYTvfYwOyv4lz@kido.flmyjkt.mongodb.net/?retryWrites=true&w=majority",
         type: process.env.DB_TYPE || "mongodb"
       },
       serverDb: {
