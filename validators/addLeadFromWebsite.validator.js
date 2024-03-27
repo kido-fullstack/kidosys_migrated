8969da599d6844e621cc84d8c3e132cc2772b9d2
ziauddin-sayyed <sayyedzia5@gmail.com>
2024-03-27 14:02:52 +0530

    summercamp template

--- validators/addLeadFromWebsite.validator.js
+++ validators/addLeadFromWebsite.validator.js
@@ -12,6 +12,7 @@ const addLeadFromWebsiteSchema = Joi.object({
     }
   ),
   child_name: Joi.string().optional().allow(''),
+  source_category: Joi.string().optional().allow(''),
   child_dob: Joi.date().format('DD/MM/YYYY').optional().allow(null),
   city: Joi.string().when(
     'form', {
