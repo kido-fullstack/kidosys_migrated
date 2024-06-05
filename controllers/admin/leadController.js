7266aaa92ed99fff249da390b31bd69f50d5045b
ziauddin-sayyed <sayyedzia5@gmail.com>
2024-06-05 14:45:39 +0530

    UTM source capture

--- controllers/admin/leadController.js
+++ controllers/admin/leadController.js
@@ -4629,7 +4629,7 @@ exports.datatableFilter = async (req, res, next) => {
     // ----------------------------LEAD PAGE datatable-----------------;
     // console.log('AAAAAAAAAAA');
     const stages = ["New Lead", "Enquiry Received", "Tour Booked", "Closed-Lead Lost", "Post Tour", "Closed-Enquiry Lost", "Closed - Won"];
-    const sortingArr = [" ","lead_no_val", "lead_date", "updatedAt", "parent_name", "child_first_name", "child_last_name", "stage_sort", "type", `${req.session.user.main && req.session.user.main == req.config.admin.main ? 'school_id.school_display_name' : 'child_first_name'}`, "source_category", "parent_know_aboutus",  "program_id.program_name",  "status_id.name","substatus_id.name", "programcategory_id.title",  "lead_no"];
+    const sortingArr = [" ","lead_no", "lead_date", "updatedAt", "parent_name", "child_first_name", "child_last_name", "stage_sort", "type", `${req.session.user.main && req.session.user.main == req.config.admin.main ? 'school_id.school_display_name' : 'child_first_name'}`, "source_category", "parent_know_aboutus",  "program_id.program_name",  "status_id.name","substatus_id.name", "programcategory_id.title",  "lead_no"];
 
     let sleFlds = {
       'lead_no': 1,
@@ -4808,15 +4808,15 @@ exports.datatableFilter = async (req, res, next) => {
     };
 
     let sortDirctn = (req.query.sSortDir_0 == 'asc') ? 1 : -1
-    let sortObj = { lead_no: sortDirctn };
+    let sortObj = { updatedAt: sortDirctn };
 
     if(req.query.iSortCol_0){
+      console.log(req.query.iSortCol_0);
       let srtInd = sortingArr[req.query.iSortCol_0];
       sortObj = {};
       sortObj[srtInd] = sortDirctn;
     }
 
-    console.log(sortObj);
 
     let lds = await Lead.find(findObj,sleFlds).sort(sortObj).limit(parseInt(req.query.iDisplayLength)).skip(parseInt(req.query.iDisplayStart));
 
