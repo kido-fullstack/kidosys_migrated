6c4a3614505008e3f5458511f4ddd803e445f78f
ziauddin-sayyed <sayyedzia5@gmail.com>
2024-06-06 19:16:52 +0530

    UTM source capture

--- controllers/admin/reportsController.js
+++ controllers/admin/reportsController.js
@@ -456,12 +456,12 @@ exports.exportLeads = async (req, res, next) => {
       });
     });
 
-    let csvData = convertToCSV(dataset)
+    // let csvData = convertToCSV(dataset)
 
-    console.log("csv responded");
-    res.setHeader('Content-Type', 'text/csv');
-    res.setHeader('Content-Disposition', 'attachment; filename="'+`Lead Export__${currentDate.format("DD-MMMM-YYYY hh:mm")}.csv`+'"');
-    return res.send(csvData);
+    // console.log("csv responded");
+    // res.setHeader('Content-Type', 'text/csv');
+    // res.setHeader('Content-Disposition', 'attachment; filename="'+`Lead Export__${currentDate.format("DD-MMMM-YYYY hh:mm")}.csv`+'"');
+    // return res.send(csvData);
 
     Promise.all(dataset)
       .then(result => {
