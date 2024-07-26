0e51a321f6ee2ac39903bc9dc7c77639e41b8c82
Chandan <chandan.kr@kidoschools.in>
2024-07-26 18:19:05 +0530

    [API] Bringing back missed API's for APK testing.

--- routes/api/programCategory/index.js
+++ routes/api/programCategory/index.js
@@ -3,12 +3,26 @@ const router = express.Router();
 const programCatController = require('../../../controllers/api/programCatController');
 const accountController = require('../../../controllers/api/accountController');
 
+router.get('/getbycenter',
+  accountController.Auth,
+  accountController.checkToken,
+  programCatController.GetProByCenter
+);
+
+router.get('/get',
+  accountController.Auth,
+  accountController.checkToken,
+  programCatController.GetProgramByCenter
+);
+
+// Duplicate API for Atto Frontend dev
 router.get('/get/by/center',
   accountController.Auth,
   accountController.checkToken,
   programCatController.GetProByCenter
 );
 
+// Duplicate API for Atto Frontend dev
 router.get('/getbycenter',
   accountController.Auth,
   accountController.checkToken,
